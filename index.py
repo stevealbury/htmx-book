ac8ad99cb991f18be1f65e99d7740eddd5868236
Steve Albury <ste@cphbusiness.dk>
2024-12-12 12:27:22 +0100

    contacts route

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask
+from flask import Flask,  redirect, render_template, request, jsonify, flash, send_file
+
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
@@ -11,8 +12,19 @@ app = Flask(__name__)
 # the associated function.
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
+def index():
+    return redirect("contacts")
+
+# show list of contacts
+@app.route('contacts')
+def contacts():
+    search = request.args.get("q")
+
+    if search is NOT None:
+        contacts_set = Contact.search(search)
+    else:
+        contacts_set = Contact.all(search)
+    return render_template("index.html", contacts = contacts_set)
 
 # main driver function
 if __name__ == '__main__':
